2d137037162e5d31228112cb22d482f8fe305480
marsell72 <baguvix2281337>
2020-09-30 18:08:14 +0500

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let split = n.toString().split('');
+    if(split[0] === '-') {
+        return +split.reverse().join('').substr(0,split.length - 1);
+    }
+    return +n.toString().split('').reverse().join('');
 }
